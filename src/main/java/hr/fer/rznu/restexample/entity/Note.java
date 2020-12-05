328f57f8b7a845b0c64df521b48e68955368e5af
jezicKarlo <karlo.jezic@gmail.com>
2020-12-05 14:45:01 +0100

    authorizeTest

--- src/main/java/hr/fer/rznu/restexample/entity/Note.java
+++ src/main/java/hr/fer/rznu/restexample/entity/Note.java
@@ -2,13 +2,10 @@ package hr.fer.rznu.restexample.entity;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class Note {
 
     private int id;
 
     private String name;
-    private List<Note> songs;
 }
--- src/test/java/hr/fer/rznu/restexample/service/RootServiceTest.java
+++ src/test/java/hr/fer/rznu/restexample/service/RootServiceTest.java
@@ -3,12 +3,14 @@ package hr.fer.rznu.restexample.service;
 import hr.fer.rznu.restexample.dto.LoginForm;
 import hr.fer.rznu.restexample.entity.User;
 import hr.fer.rznu.restexample.repository.UserRepository;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RootServiceTest {
 
@@ -16,28 +18,39 @@ class RootServiceTest {
     public void authenticateTest() {
         User user = createUser();
         LoginForm loginForm = createLoginForm();
-
         UserRepository repository = Mockito.mock(UserRepository.class);
-        Mockito.when(repository.getByUsername("karlo")).thenReturn(user);
+
+        Mockito.when(repository.getByUsername("kjezic")).thenReturn(user);
 
         RootService service = new RootService(repository);
         UUID uuid = service.authenticate(loginForm);
         assertNotNull(uuid);
     }
 
+    @Test
+    public void authorizeTest() {
+        User user = createUser();
+        UserRepository repository = Mockito.mock(UserRepository.class);
+        Mockito.when(repository.getUserById(1)).thenReturn(user);
+
+        RootService service = new RootService(repository);
+        assertTrue(service.authorize(user.getUUID(), 1));
+    }
+
     private LoginForm createLoginForm() {
         LoginForm loginForm = new LoginForm();
-        loginForm.setUsername("karlo");
+        loginForm.setUsername("kjezic");
         loginForm.setPassword("1234");
         return loginForm;
     }
 
-    private User createUser() {
+    private static User createUser() {
         User user = new User();
         user.setFirstName("Karlo");
         user.setLastName("Jezic");
         user.setId(1);
         user.setPassword("1234");
+        user.setUsername("kjezic");
         user.setUUID(UUID.randomUUID());
         return user;
     }
