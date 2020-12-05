a716a1d185bc14a7a00d75815b4931970cc07394
jezicKarlo <karlo.jezic@gmail.com>
2020-12-05 18:25:42 +0100

    remove unused imports

--- src/main/java/hr/fer/rznu/restexample/repository/UserRepository.java
+++ src/main/java/hr/fer/rznu/restexample/repository/UserRepository.java
@@ -2,9 +2,6 @@ package hr.fer.rznu.restexample.repository;
 
 import hr.fer.rznu.restexample.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
